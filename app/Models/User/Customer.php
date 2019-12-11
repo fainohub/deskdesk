0350ef73376198f2c77e8798379bf9563854d0bb
Thiago Faino <thiagofaino@gmail.com>
2019-12-11 16:54:03 -0300

    Create agents, departments and customers migrations

--- app/Models/User/Customer.php
+++ app/Models/User/Customer.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models\Customer;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class Customer extends Authenticatable
 {
     use Notifiable;
+
+    protected $table = 'customer';
 
     /**
      * The attributes that are mass assignable.
@@ -28,12 +29,4 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
