9a51ea4aa2029e957e82f098f01eade82b4caf41
Thiago Faino <thiagofaino@gmail.com>
2019-12-16 18:07:12 -0300

    Fix test

--- tests/Feature/AgentTicketMessageCreateTest.php
+++ tests/Feature/AgentTicketMessageCreateTest.php
@@ -3,31 +3,18 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-<<<<<<< HEAD
-use App\Models\Customer;
-use App\Models\TicketMessage;
-
-class CustomerTicketMessageCreateTest extends TestCase
-{
-    private $customer;
-=======
 use App\Models\Agent;
 use App\Models\TicketMessage;
 
 class AgentTicketMessageCreateTest extends TestCase
 {
     private $agent;
->>>>>>> release/1.5.0
 
     protected function setUp(): void
     {
         parent::setUp();
 
-<<<<<<< HEAD
-        $this->customer = factory(Customer::class)->create();
-=======
         $this->agent = factory(Agent::class)->create();
->>>>>>> release/1.5.0
     }
 
     public function testCreateMessageSuccess()
@@ -39,18 +26,10 @@ class AgentTicketMessageCreateTest extends TestCase
         ];
 
         $response = $this
-<<<<<<< HEAD
-            ->actingAs($this->customer, 'customer')
-            ->from(route('customer.tickets.show', ['id' => $ticketMessage->ticket_id]))
-            ->post(route('customer.tickets.message.store', ['id' => $ticketMessage->ticket_id]), $data);
-
-        $response->assertRedirect(route('customer.tickets.show', ['id' => $ticketMessage->ticket_id]));
-=======
             ->actingAs($this->agent, 'agent')
             ->from(route('agent.tickets.show', ['id' => $ticketMessage->ticket_id]))
             ->post(route('agent.tickets.message.store', ['id' => $ticketMessage->ticket_id]), $data);
 
         $response->assertRedirect(route('agent.tickets.show', ['id' => $ticketMessage->ticket_id]));
->>>>>>> release/1.5.0
     }
 }
