7be5d9d5370b0570126c3fb285378092033bd6c2
Thiago Faino <thiagofaino@gmail.com>
2019-12-16 22:50:27 -0300

    - remove .idea folder - fix pnotify import map

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,10 +7,12 @@ const mix = require('laravel-mix');
  */
 
 mix.copy('node_modules/pnotify/dist/iife/PNotify.js', 'public/js/PNotify.js');
+mix.copy('node_modules/pnotify/dist/iife/PNotify.js.map', 'public/js/PNotify.js.map');
 mix.copy('node_modules/pnotify/dist/iife/PNotifyButtons.js', 'public/js/PNotifyButtons.js');
-mix.copy('resources/theme/css/style.css', 'public/css/style.css');
+mix.copy('node_modules/pnotify/dist/iife/PNotifyButtons.js.map', 'public/js/PNotifyButtons.js.map');
 
 mix.copyDirectory('resources/images', 'public/images');
+mix.copyDirectory('resources/theme/css', 'public/css');
 mix.copyDirectory('resources/theme/fonts', 'public/fonts');
 mix.copyDirectory('resources/theme/js', 'public/js');
 mix.copyDirectory('resources/theme/vendors', 'public/vendors');
